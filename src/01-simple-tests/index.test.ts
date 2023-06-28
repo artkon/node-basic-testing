ccfc4003c035a70edb901f2f97d565a673b7ef97
artkon <artemkonopelko@gmail.com>
2023-06-28 19:09:47 +0400

    test(simple-tests): implement 01-simple-tests

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -1,32 +1,59 @@
-// Uncomment the code below and write your tests
-// import { simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 describe('simpleCalculator tests', () => {
   test('should add two numbers', () => {
-    // Write your test here
+    const props = { a: 1, b: 2, action: Action.Add };
+    const expectedResult = 3;
+    const result = simpleCalculator(props);
+
+    expect(result).toBe(expectedResult);
   });
 
   test('should subtract two numbers', () => {
-    // Write your test here
+    const props = { a: 1, b: 2, action: Action.Subtract };
+    const expectedResult = -1;
+    const result = simpleCalculator(props);
+
+    expect(result).toBe(expectedResult);
   });
 
   test('should multiply two numbers', () => {
-    // Write your test here
+    const props = { a: 2, b: 3, action: Action.Multiply };
+    const expectedResult = 6;
+    const result = simpleCalculator(props);
+
+    expect(result).toBe(expectedResult);
   });
 
   test('should divide two numbers', () => {
-    // Write your test here
+    const props = { a: 6, b: 3, action: Action.Divide };
+    const expectedResult = 2;
+    const result = simpleCalculator(props);
+
+    expect(result).toBe(expectedResult);
   });
 
   test('should exponentiate two numbers', () => {
-    // Write your test here
+    const props = { a: 2, b: 3, action: Action.Exponentiate };
+    const expectedResult = 8;
+    const result = simpleCalculator(props);
+
+    expect(result).toBe(expectedResult);
   });
 
   test('should return null for invalid action', () => {
-    // Write your test here
+    const props = { a: 2, b: 3, action: 'invalid action' };
+    const expectedResult = null;
+    const result = simpleCalculator(props);
+
+    expect(result).toBe(expectedResult);
   });
 
   test('should return null for invalid arguments', () => {
-    // Write your test here
+    const props = { a: 'a', b: 'b', action: Action.Add };
+    const expectedResult = null;
+    const result = simpleCalculator(props);
+
+    expect(result).toBe(expectedResult);
   });
 });
